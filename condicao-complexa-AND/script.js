9922bbce7a7683d40687078877d27d303804fe30
10S01610 <sophiaalmeidda777@gmail.com>
2026-03-18 13:20:27 -0300

    meu primeiro commit

---
+++ condicao-complexa-AND/script.js
@@ -0,0 +1,7 @@
+let idade = Number(prompt("insira sua idade"));
+let temCNH = prompt("Possui CNH? (sim ou não)")
+if (idade >= 18) {
+    alert("Pode dirigir")
+} else{
+    alert("Não pode dirigir")
+}
